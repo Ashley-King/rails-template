ef7479c7897a56a32ad756da38e356f4b780aa95
ashley <ashleyot@gmail.com>
2022-11-18 07:46:13 -0500

    fixed has_rich_text calls in posts model - only use one per field

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  has_rich_text :body, :author_bio, :references_section
+  has_rich_text :body 
+  has_rich_text :author_bio 
+  has_rich_text :references_section
 end
